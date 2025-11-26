426510ac568b070a68a91034d0d5c2cc69ac7702
yoonjeong <ahffkd7721@naver.com>
2025-11-26 10:31:18 +0900

    xxxx

--- DAY14/index.js
+++ DAY14/index.js
@@ -1,35 +1,70 @@
 export default async function main() {
-    console.log('start app')
+    console.log('start App')  
+
+
+    const mainMenu = document.querySelector('#main-menu')
+    // const newGame = document.querySelector('')
+    const creditScreen = document.querySelector('#credit-screen')
+    let currentScreen = "mainMenu"
+
+    
     let currentIndex = 0;
     const menuitems = document.querySelectorAll(".menu-item");
 
     console.log(menuitems.length)
-    const menuitems_count = menuitems.length
+    const menuitem_count = menuitems.length
 
     menuitems[currentIndex].classList.add("select")
 
     window.addEventListener("keydown", (e) => {
-        console.log(e.key)
-        menuitems[currentIndex].classList.remove("select")
+        menuitems[currentIndex].classList.remove('select')
 
-        if(e.key == "ArrowUp"){
-        currentIndex--
-        if (currentIndex < 0 ) {
-        currentIndex = menuitem_count-1
-        }
-    }
+        if(currentScreen == "mainMenu") {
 
-        else if(e.key == "ArrowDown"){
-        currentIndex++
-        currentIndex %=menuitem_count
+            console.log(e.key)
+            if(e.key == "ArrowUp") {
+                currentIndex--
+                if(currentIndex < 0) {
+                    currentIndex = menuitem_count-1
+                }
+            }
+            else if(e.key == "ArrowDown") {
+                currentIndex++
+                currentIndex %=menuitem_count
+            }
+            else if(e.key == "Enter") {
+                console.log(menuitems[currentIndex].dataset.action)
+                const select_action = menuitems[currentIndex].dataset.action
+
+                if(select_action == 'credit') {
+                    mainMenu.classList.add('hide')
+                    creditScreen.classList.remove('hide')
+                    currentScreen = "creditScreen"
+                }
+            }
+
+            console.log(currentIndex)
+            menuitems[currentIndex].classList.add('select')
+            
+
         }
-        else if(e.key == "Enter"){
-            console.log(menuitems[currentIndex])
+        else if(currentScreen == "creditScreen") {
+
+            console.log('credit screen')
+
+            if(e.key == "Enter") {
+                
+                creditScreen.classList.add('hide')
+                mainMenu.classList.remove('hide')
+                currentScreen = "mainMenu"
+            }
         }
+
         
+
     })
-    console.log[currentIndex].classList.add('select')
-    menuitems[currentIndex].classList.add("select")
+
+    
+
+
 }
-
-
